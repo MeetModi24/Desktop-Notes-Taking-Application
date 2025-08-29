0d5e03cd7888c96dc477bdffcb5957aaf92db56a
MeetModi24 <modimeet20@gmail.com>
2025-08-29 09:46:42 +0530

    react setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+// src/App.jsx
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 text-gray-900">
+      <h1 className="text-3xl font-bold underline">
+        Notes App Frontend + Electron + Tailwind
+      </h1>
+    </div>
+  );
+}
